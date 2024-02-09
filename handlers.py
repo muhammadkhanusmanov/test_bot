c165485006201c661fc5fb7775067df36eb9fbb9
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-02-09 23:37:45 +0500

    added handler for check

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters,CallbackQueryHandler,ConversationHandler
 from mainfuncs import (
-    start
+    start,userfun,check
 )
 
 
@@ -8,6 +8,7 @@ updater=Updater('5655855014:AAFgMvIpeQ4CSn9N-q547jvM5YL286vfrT4')
 
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('start',start))
+dp.add_handler(CallbackQueryHandler(userfun,pattern='user'))
 
 
 updater.start_polling()
