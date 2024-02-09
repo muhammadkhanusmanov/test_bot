33627e0499144a58f5babb967b4a07705ed345d6
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-02-09 23:37:12 +0500

    user check func

--- mainfuncs.py
+++ mainfuncs.py
@@ -5,6 +5,8 @@ from telegram import (
     )
 import sqlite3
 
+bot = Bot('5655855014:AAFgMvIpeQ4CSn9N-q547jvM5YL286vfrT4')
+
 def start(update:Update, context:CallbackContext):
     cnt = sqlite3.connect('data.db')
     cr = cnt.cursor()
@@ -23,11 +25,52 @@ def start(update:Update, context:CallbackContext):
         btn = InlineKeyboardMarkup([[btn2,btn1], [btn3,btn4]])
     else:
         command = f"""
+        SELECT * FROM Users WHERE chat_id = "{chat_id}"
+        """
+        b = cr.execute(command).fetchall()
+        if not b:
+            command = f"""
+            INSERT INTO Users (chat_id) VALUES ("{chat_id}")
+            """
+            cr.execute(command)
+            cnt.commit()
+        command = f"""
         SELECT chat_id FROM Users WHERE id = {1}
         """
-        channel = cr.execute(command)[0]
+        channel = cr.execute(command).fetchone()[0]
         text = "Assalomu alaykum botga xush kelibsiz, botdan to'liq foydalanish uchun majburiy obunani bajaring."
         btn1 = InlineKeyboardButton('Obuna ➕',callback_data='obuna',url=f'https://t.me/{channel[1:]}')
         btn2 = InlineKeyboardButton('Tekshirish',callback_data='user obuna')
         btn = InlineKeyboardMarkup([[btn1],[btn2]])
     bot.sendMessage(chat_id,text,reply_markup=btn)
+
+def check(chat_id,bot,channel):
+    chan1=bot.getChatMember(channel,str(chat_id))['status']
+    if chan1=='left':
+        return False
+    return True
+
+def userfun(update:Update, context:CallbackContext):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    msg = query.message.message_id
+    bot=context.bot
+    b = query.data.split(' ')[1]
+    cnt = sqlite3.connect('data.db')
+    cr = cnt.cursor()
+    command = f"""
+     SELECT chat_id FROM Users WHERE id = {1}
+    """
+    channel = cr.execute(command).fetchone()[0]
+    a = check(chat_id,bot,channel)
+    bot.delete_message(chat_id,msg)
+    if a:
+        text = "Obuna mufavaqiyatli amalga oshirildi!\nBo'limlardan birini tanlang."
+        btn1 = InlineKeyboardButton("Test yaratish",callback_data='test +')
+        btn2 = InlineKeyboardButton("Test yechish",callback_data='test tek')
+        btn = InlineKeyboardMarkup([[btn1,btn2]])
+        bot.sendMessage(chat_id,text,reply_markup=btn)
+    else:
+        bot.sendMessage(chat_id,'Obuna bo\'lishda xatolik!')
+
+
