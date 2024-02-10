dbe40acf770aef10c9bbb53fa6236ae1cf898388
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-02-10 10:34:45 +0500

    handler for addobuna

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters,CallbackQueryHandler,ConversationHandler
 from mainfuncs import (
-    start,userfun,check,adminstng,addadmin,deladmin
+    start,userfun,check,adminstng,addadmin,deladmin,addobuna
 )
 
 
@@ -12,6 +12,7 @@ dp.add_handler(CallbackQueryHandler(userfun,pattern='user'))
 dp.add_handler(CallbackQueryHandler(adminstng,pattern='admin'))
 dp.add_handler(MessageHandler(Filters.regex(r'^admin\+'),addadmin))
 dp.add_handler(MessageHandler(Filters.regex(r'^admin\-'),deladmin))
+dp.add_handler(MessageHandler(Filters.regex(r'^obuna\+'),addobuna))
 
 updater.start_polling()
 updater.idle()
