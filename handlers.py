b43bf4955862f4e6e7f3f7312c6369f25344df8c
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-02-10 21:23:24 +0500

    handler for test

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters,CallbackQueryHandler,ConversationHandler
 from mainfuncs import (
-    start,userfun,check,adminstng,addadmin,deladmin,addobuna
+    start,userfun,check,adminstng,addadmin,deladmin,addobuna,test
 )
 
 
@@ -13,6 +13,7 @@ dp.add_handler(CallbackQueryHandler(adminstng,pattern='admin'))
 dp.add_handler(MessageHandler(Filters.regex(r'^admin\+'),addadmin))
 dp.add_handler(MessageHandler(Filters.regex(r'^admin\-'),deladmin))
 dp.add_handler(MessageHandler(Filters.regex(r'^obuna\+'),addobuna))
+dp.add_handler(CallbackQueryHandler(test,pattern='test'))
 
 updater.start_polling()
 updater.idle()
